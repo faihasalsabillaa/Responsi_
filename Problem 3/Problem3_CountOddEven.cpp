4433911ce6def463b0850231f92556db2b0a06eb
faihasalsabillaa <rfsr2006@gmail.com>
2024-11-25 17:02:49 +0700

    Add files via upload

---
+++ Problem 3/Problem3_CountOddEven.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+//Q.3 Count odd and even numbers
+//Ratu Faiha Salsabilla Rahmadina_532756
+
+int countOddEven(input &count);
+int countOdd()
+for (&count) {
+    int oddCount = 0, evenCount = 0;
+    if (count %2 == 0) {
+    CountEven++;
+    else 
+    CountOdd++;
+    }
+}
+
+cout << "The total of even numbers is: " << countEven << endl;
+cout << "The total of odd numbers is: " << countOdd << endl;
+
+int main() {
+    arr = {1,2,3,4,5,6,7,8,9,10}
+}
